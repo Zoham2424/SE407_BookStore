1d23c8a894fb2e1be01909ba9aafbd6423181a6f
Ian A <iaalvaradocubero@email.neit.edu>
2025-02-07 12:52:50 -0500

    Add project files.

---
+++ BookStore.Test/BasicFuntionsTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookStore.Models;
+
+
+namespace BookStore.Test
+{
+       public class BasicFuntionsTest
+    {
+        [Fact]
+        public void TestGetBookByTitle()
+        {
+            // Act
+            Book? testBook = BasicFunctions.GetBookByTitle("");
+
+            // Assert
+            Assert.NotNull(testBook);
+            Assert.Equal("", testBook.Title);
+        }
+
+        
+    }
+}
---
+++ SE407_BookStore/BasicFuntions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SE407_BookStore
+{
+    internal class BasicFuntions
+    {
+        public static Book? GetBookByTitle(string title)
+        {
+            using (var context = new )
+            {
+                return context.Books
+                    .FirstOrDefault(b => b.Title == title);
+            }
+        }
+        public static List<Book> GetAllBooks()
+        {
+            using (var context = new )
+            {
+                return context.Books.ToList();
+            }
+        }
+        public static List<Book> GetBooksByAuthorLastName(string lastName)
+        {
+            using (var context = new )
+            {
+                return context.Authors
+                    .Where(a => a.LastName == lastName)
+                    .Join(
+                        context.Books,
+                        author => author.AuthorId,
+                        book => book.AuthorId,
+                        (author, book) => book
+                    )
+                    .ToList();
+            }
+        }
+    }
+}
